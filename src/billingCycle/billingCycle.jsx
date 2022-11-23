f6c7a49bcf96df73fb8626b0b3073b8e25138fa0
MateusGomes04 <mateusgomesc04@gmail.com>
2022-11-23 12:41:39 -0300

    add tabHeader Component

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,18 +1,24 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
+import TabHeader from "../common/tab/tabHeader";
 
 class BillingCycle extends Component {
     render() {
-        return ( 
+        return (
             <div>
-                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro'/>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
                 <Content>
                     <Tabs>
                         <TabsHeader>
+                            <TabHeader label="listar" icon="bars" target="tabList" />
+                            <TabHeader label="Incluir" icon="plus" target="tabCreate" />
+                            <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+                            <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
+
 
                         </TabsHeader>
                         <TabsContent>
