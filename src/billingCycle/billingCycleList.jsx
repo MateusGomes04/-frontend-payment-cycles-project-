0c23d2d050eaef0f85dc1d827883c2665b517d44
MateusGomes04 <mateusgomesc04@gmail.com>
2022-11-24 11:14:40 -0300

    Displaying Data in Billing Cycle List

--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -1,13 +1,24 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { getList} from "./billingCycleActions"
+import { getList } from "./billingCycleActions"
 
 class BillingCycleList extends Component {
 
     componentWillMount() {
         this.props.getList()
         console.log(this.props.list)
+    }
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
     }
 
     render() {
@@ -22,13 +33,13 @@ class BillingCycleList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
         )
     }
 }
-const mapStateToProps = state => ({list: state.billingCycle.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
-export default connect(mapStateToProps, mapDispatchToProps) (BillingCycleList)
+const mapStateToProps = state => ({ list: state.billingCycle.list })
+const mapDispatchToProps = dispatch => bindActionCreators({ getList }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
