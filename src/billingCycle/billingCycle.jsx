96d57ef161cbeb66e710070a110924cba91a398e
MateusGomes04 <mateusgomesc04@gmail.com>
2022-11-23 22:31:29 -0300

    add Visibility tab

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -9,12 +9,13 @@ import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
 import TabContent from "../common/tab/tabContent";
-import {selectTab} from '../common/tab/tabActions'
+import {selectTab, showTabs} from '../common/tab/tabActions'
 
 class BillingCycle extends Component {
 
     componentWillMount() {
-        this.props.selectTab('tabList')
+        this.props.selectTab("tabList")
+        this.props.showTabs("tabList", "tabCreate")
     }
     render() {
         return (
@@ -41,5 +42,5 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionsCreators({selectTab}, dispatch)
+const mapDispatchToProps = dispatch => bindActionsCreators({selectTab, showTabs}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
