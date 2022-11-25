c8cbfafeefd21b69eade8ae0b81b93b59b3ed309
MateusGomes04 <mateusgomesc04@gmail.com>
2022-11-25 09:41:10 -0300

    Displaying Success and Error Messages

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -5,7 +5,7 @@ import Header from '../common/template/header';
 import SideBar from '../common/template/sideBar';
 import Footer from '../common/template/footer';
 import Routes from './routes';
-
+import Messages from '../common/msg/messages';
 
 
 export default props => (
@@ -16,5 +16,6 @@ export default props => (
             <Routes />
         </div>
         <Footer />
+        <Messages />
     </div>
 )
--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,15 +1,16 @@
 import { combineReducers } from "redux";
 import {reducer as formReducer} from "redux-form"
-
-import dashboardReducer from "../dashboard/dashboardReducer";
-import tabReducer from "../common/tab/tabReducer";
-import billingCycleReducer from "../billingCycle/billingCycleReducer";
+import {reducer as toastrReducer} from "react-redux-toastr"
+import DashboardReducer from "../dashboard/dashboardReducer";
+import TabReducer from "../common/tab/tabReducer";
+import BillingCycleReducer from "../billingCycle/billingCycleReducer";
 
 const rootReducer = combineReducers({
-    dashboard: dashboardReducer,
-    tab: tabReducer,
-    billingCycle: billingCycleReducer,
-    form: formReducer
+    dashboard: DashboardReducer,
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer,
+    form: formReducer,
+    toastr: toastrReducer
 })
 
 export default rootReducer
