d4946ae78650303ceb7315af7861ab15c33a2bc9
MateusGomes04 <mateusgomesc04@gmail.com>
2022-11-30 12:14:05 -0300

    add AuthOrAp

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Router, Route, IndexRoute, Redirect, hashHistory } from "react-router";
 import Dashboard from "../dashboard/dashboard";
-import BillingCycle from "../billingCycle/billingCycle"
-import App from './app'
+import BillingCycle from "../billingCycle/billingCycle";
+import AuthOrApp from "./authOrApp";
 
-export default props => (
-    <Router history={hashHistory}>
-        <Route path='/' component={App}>
-            <IndexRoute component={Dashboard} />
-            <Route path='billingCycles' component={BillingCycle} />
-        </Route>
-        <Redirect from="*" to="/" />
-    </Router>
-
-)
+export default (props) => (
+  <Router history={hashHistory}>
+    <Route path="/" component={AuthOrApp}>
+      <IndexRoute component={Dashboard} />
+      <Route path="billingCycles" component={BillingCycle} />
+    </Route>
+    <Redirect from="*" to="/" />
+  </Router>
+);
